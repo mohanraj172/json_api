91dc68aaf4bbe8bed140b0e1b245bda7f256b41b
mohanraj172 <0503mohan@gmail.com>
2024-10-12 23:24:41 +0530

    update the file

--- asstes/script.js
+++ asstes/script.js
@@ -1,7 +1,9 @@
 const tbody = document.getElementById('tbody');
 const ID = document.getElementById('id');
-const myName = document.getElementById('name')
-const Username = document.getElementById('username')
+const myName = document.getElementById('name');
+const Username = document.getElementById('username');
+const submitBtn = document.getElementById('submitBtn');
+const Updatabtn = document.getElementById('updateBtn');
 
 const addRow = (id,completed,title) => {
 
@@ -10,7 +12,8 @@ const addRow = (id,completed,title) => {
     <td>${id}</td>
     <td>${completed}</td>
     <td>${title}</td>
-    <td><button class="btn btn-danger">EDIT</button></td>
+    <td><button onclick="editUser(${id},${completed},${title})"class="btn btn-primary">EDIT</button>
+    <button class="btn btn-danger" onclick="handleDelete(${id})">Delete</button></td>
     </tr>
     `
 }
@@ -19,6 +22,7 @@ async function getData(){
     const response = await url.json();
     response.map((datas)=>{
         addRow(datas.id,datas.name,datas.username);
+
     })
 }
 async function postData(){
@@ -37,16 +41,34 @@ async function postData(){
         body: JSON.stringify(userData)
     })
 }
+ function editUser(id,name,username){
+    alert(hello)
+        userId.value =id;
+        useremail.value= name;
+       userName.value=username;
+       submitBtn.style.display='none'
+       Updatabtn.style.display='block'
+  }
 
-async function putData(){
-    const url = await fetch('http://localhost:3000/users',{
-        method: 'PUT',
-        headers: {
-            'content-Type' : 'Application/json'
-        },
-        body: JSON.stringify()
+// const editUser ={
+//     id :`${ID}`,
+//     name :`${emailname.value}`,
+//     username :`${username.value}`
+// }
+// putData(editUser)
+// console.log(editUser);
+
+// async function putData(){
+//     const url = await fetch('http://localhost:3000/users${Id}',{
+//         method: 'PUT',
+//         headers: {
+//             'content-Type' : 'Application/json'
+//         },
+//         body: JSON.stringify()
+//     })
+// }
+async function handleDelete(id){
+    const url = await fetch(`http://localhost:3000/users/${id}`,{
+        method: 'DELETE'
     })
 }
-putData()
-
-
